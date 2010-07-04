8fa6f0e3495454f6976165cca2d2fab79d6e0dc4
Steve Loveless <steve.loveless@gmail.com>
2010-07-04 12:35:02 -0700

    Added revision method & (lame) test

--- spec/client/legacy_spec.rb
+++ spec/client/legacy_spec.rb
@@ -1,8 +1,8 @@
-require File.dirname(__FILE__) + '../spec_helper.rb'
+require File.dirname(__FILE__) + '/../spec_helper.rb'
 require 'fisheye-crucible/client/legacy'
 
 describe FisheyeCrucible::Client::Legacy do
-  include FisheyeCrucible::Client
+  include FisheyeCrucible
 
   GOOD_USERNAME = 'gemtest'
   GOOD_PASSWORD = 'gemtest'
@@ -21,6 +21,16 @@ describe FisheyeCrucible::Client::Legacy do
     it "should login successfully with good credentials" do
       token = @fc.login(GOOD_USERNAME, GOOD_PASSWORD)
       token.should match(/^\w+:\d{3}:\w{32}$/)
+    end
+  end
+
+  context "revision" do
+    REVISION_RESPONSE =<<-EOF
+<response><revision path="README.txt" rev="5774" author="parrt" date="2009-03-01T22:40:41.00Z" state="deleted" totalLines="0" linesAdded="0" linesRemoved="123" csid="5774" ancestor="5646"><log>mking tool dir</log></revision>
+</response>
+      EOF
+    it "should return a Hash of properties" do
+      @fc.revision('antlr', '', '5774').class.should == Hash
     end
   end
 end
