ec92d00913095c8e8ababf362d2cc668ce631830
Steve Loveless <steve.loveless@gmail.com>
2010-07-04 14:01:41 -0700

    Added quick and dirty ability to do debug messages

--- lib/fisheye-crucible/client.rb
+++ lib/fisheye-crucible/client.rb
@@ -4,6 +4,9 @@ require 'rest-client'
 
 class FisheyeCrucible::Client
 
+  # @return [Boolean] Turn debug on or off
+  attr_accessor :do_debug
+
   ##
   # Sets up a Rest object to interact with the server(s).
   # 
@@ -12,5 +15,16 @@ class FisheyeCrucible::Client
     @server = server
     @fisheye_rest = RestClient::Resource.new(@server)
     @token = nil
+    @do_debug = false
+  end
+
+  ##
+  # Print out string if debug is turned on.
+  # 
+  # @param [String] string The debug string to print out.
+  def debug(string)
+    if @do_debug
+      puts string
+    end
   end
 end
