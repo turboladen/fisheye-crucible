65d38dd195c96a053f7733716fb6936367a7298b
Steve Loveless <steve.loveless@gmail.com>
2010-08-05 17:39:25 -0700

    Bumped version for up-coming bug fix

--- lib/fisheye-crucible.rb
+++ lib/fisheye-crucible.rb
@@ -5,6 +5,6 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'fisheye_crucible_exception'
 
 module FisheyeCrucible
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   WWW = 'http://github.com/turboladen/fisheye-crucible'
 end
