5505943fa9716474624ca39975cd8a7636405e81
Steve Loveless <steve.loveless@gmail.com>
2011-03-08 23:26:05 -0800

    Fixed require paths

--- lib/fisheye-crucible.rb
+++ lib/fisheye-crucible.rb
@@ -1,2 +1,2 @@
-require './fisheye-crucible/version'
-require './fisheye_crucible_exception'
+require File.expand_path(File.dirname(__FILE__) + '/fisheye-crucible/version')
+require File.expand_path(File.dirname(__FILE__) + '/fisheye-crucible/error')
